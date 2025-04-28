3d857c1082eec09c785207db0b0ec1537f460135
Anton Lobarev <74120447+tonyloba@users.noreply.github.com>
2025-04-28 02:02:31 +0200

    PS05 Scrapy Divan.ru

---
+++ PS05_divanpars/PS05_divanpars/spiders/divannewpars.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+    def parse(self, response):
+        svets = response.css('div._Ud0k')
+        for svet in svets:
+            yield {
+                'name': svet.css('div.lsooF span::text').get(),
+                'price': svet.css('div.pY3d2 span::text').get(),
+                'link': svet.css('a::attr(href)').get()
+
+            }
